7a97d656b1d4099731b3032d1dc6ed0466d2d233
Aurelien St <aurelien.strimelle@bstorm.be>
2026-01-29 15:54:06 +0100

    EFCore config

---
+++ Demo_WebAPI_EventAgenda.Infrastructure.Database/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Demo_WebAPI_EventAgenda.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo_WebAPI_EventAgenda.Infrastructure.Database
+{
+    // Configuration de la base de donnée pour EFCore
+    public class AppDbContext : DbContext
+    {
+        // Ensemble des tables
+        public DbSet<AgendaEvent> AgendaEvents { get; set; }
+        public DbSet<EventCategory> EventCategories { get; set; }
+
+
+
+    }
+}

b896fe4f58b5398565ca84cad493c158372297c2
Aurelien St <aurelien.strimelle@bstorm.be>
2026-01-29 14:11:50 +0100

    Interface repository

---
+++ Demo_WebAPI_EventAgenda.ApplicationCore/Interfaces/IEventAgendaRepository.cs
@@ -0,0 +1,14 @@
+﻿using Demo_WebAPI_EventAgenda.Domain.Models;
+
+namespace Demo_WebAPI_EventAgenda.ApplicationCore.Interfaces
+{
+    public interface IEventAgendaRepository
+    {
+        AgendaEvent GetById(long id);
+        IEnumerable<AgendaEvent> GetMany(int offset, int limit);
+
+        AgendaEvent Insert(AgendaEvent data);
+        AgendaEvent Update(long id, AgendaEvent data);
+        bool Delete(long id);
+    }
+}

cc56ba4bf836cc04dd1cd04efe6f13296a437703
Aurelien St <aurelien.strimelle@bstorm.be>
2026-01-30 12:06:39 +0100

    Dependency injection

--- Demo_WebAPI_EventAgenda.Presentation.WebAPI/Program.cs
+++ Demo_WebAPI_EventAgenda.Presentation.WebAPI/Program.cs
@@ -1,6 +1,29 @@
+﻿using Demo_WebAPI_EventAgenda.ApplicationCore.Interfaces;
+using Demo_WebAPI_EventAgenda.Infrastructure.Database;
+using Demo_WebAPI_EventAgenda.Infrastructure.Database.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container (→ Injection de dependance)
+// Configuration des service possible avec les méthodes suivantes : 
+// - AddSingleton : Créer une instance une seul fois et la garde en mémoire.
+// - AddScoped : Créer une instance par requete
+// - AddTransient : Créer une instance a chaque demande
+
+// DI Configuration 
+// - Services (TODO)
+//   ...
+// - Repository
+builder.Services.AddScoped<IAgendaEventRepository, AgendaEventRepository>();
+//   ...
+// - DB Context
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer("Connection sera présente ici apres la pause 🍔");
+});
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
